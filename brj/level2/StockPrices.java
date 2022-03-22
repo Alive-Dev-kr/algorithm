f43e09da8b86ea7b53d0dadabcda9744b9a8729e
bhr0554 <bhr5004@gmail.com>
2022-03-22 17:25:06 +0900

    [추가] - level2 스택/큐 주식가격 파일 생성

---
+++ brj/level2/StockPrices.java
@@ -0,0 +1,15 @@
+package level2;
+
+public class StockPrices {
+
+    public int[] solution(int[] prices) {
+        int[] answer = {};
+
+
+        return answer;
+    }
+
+    public static void main(String args[]) {
+
+    }
+}
