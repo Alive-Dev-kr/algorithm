4ade3fec64c015ce805d2001c6b36a1cc279d27d
bhr0554 <bhr5004@gmail.com>
2022-06-03 11:48:04 +0900

    [수정] - 카카오 오픈 채팅방 완료(마지막이 Leave일 경우 처리)

--- brj/level2/KakaoOpenChat.java
+++ brj/level2/KakaoOpenChat.java
@@ -12,32 +12,40 @@ public class KakaoOpenChat {
         List<String> recodList = Arrays.asList(record);
         Collections.reverse(recodList);
 
+        // userId , result list index
+        Map<String, Integer> map = new HashMap<>();
+
         String[] recodSplt = null;
         String rsltStr = null;
-        String split1, split2, split3  = null; // Sort, userId
+        String split1, split2, nickName  = null; // Sort, userId
         for(String str : recodList) {
             recodSplt = str.split(" ");
 
             split1 = recodSplt[0];
             split2 = recodSplt[1];
-            split3 = recodSplt.length==3 ? recodSplt[2] : "";
 
-            if(split2.length() <= 10 && split3.length() <= 10) {
-                if (!userMap.containsKey(split2) && (split1.equals("Change") || split1.equals("Enter"))) {
-                    userMap.put(split2, split3);
-                }
+            if (userMap.get(split2) == null && (split1.equals("Change") || split1.equals("Enter"))) {
+                userMap.put(split2, recodSplt[2]);
+            }
 
-                rsltStr = userMap.get(split2) + "님이 ";
-                switch (split1.charAt(0)) {
-                    case 'E':
-                        rsltStr += "들어왔습니다.";
-                        result.add(rsltStr);
-                        break;
-                    case 'L':
-                        rsltStr += "나갔습니다.";
-                        result.add(rsltStr);
-                        break;
-                }
+            nickName = userMap.get(split2);
+            rsltStr = nickName + "님이 ";
+            switch (split1.charAt(0)) {
+                case 'E':
+                    rsltStr += "들어왔습니다.";
+                    result.add(rsltStr);
+                    break;
+                case 'L':
+                    rsltStr += "나갔습니다.";
+                    result.add(rsltStr);
+                    if(nickName == null) {
+                        map.put(split2, result.size()-1);
+                    }
+                    break;
+            }
+            if(map.get(split2) != null && userMap.get(split2) != null) {
+                String replaceNull = result.get(map.get(split2)).replaceAll("null", userMap.get(split2));
+                result.set(map.get(split2), replaceNull);
             }
         }
 
@@ -56,7 +64,8 @@ public class KakaoOpenChat {
     public static void main (String args[]) {
         //String[] record = {"Enter uid1234 Muzi", "Enter uid4567 Prodo", "Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"};
         //String[] record = {"Enter uid1234 Muzi", "Enter uid4567 Prodo", "Leave uid1234","Change uid4567 Ryan","Enter uid1234 Prodo"};
-        String[] record = {"Enter uid1234 Muzi", "Enter uid4567 Prodo", "Leave uid1234","Change uid4567 Ryan","Enter uid1234 Prodo", "Enter uid1232 New"};
+        //String[] record = {"Enter uid1234 Muzi", "Enter uid4567 Prodo", "Leave uid1234","Change uid4567 Ryan","Enter uid1234 Prodo", "Enter uid1232 New"};
+        String[] record = {"Enter uid1234 Muzi", "Enter uid4567 Prodo", "Leave uid1234","Change uid4567 Ryan","Enter uid1234 Prodo", "Enter uid1232 New", "Leave uid4567"};
 
         solution(record);
     }
