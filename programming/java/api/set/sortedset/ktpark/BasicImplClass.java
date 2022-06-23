ec19e03211c27b6531432e110f57f0be70c109eb
bhr0554 <bhr5004@gmail.com>
2022-06-23 19:03:14 +0900

    [수정] - 이론 폴더 수정

---
+++ programming/java/api/set/sortedset/ktpark/BasicImplClass.java
@@ -0,0 +1,17 @@
+package java.api.set.sortedset.ktpark;
+
+public class BasicImplClass implements BasicInterface {
+
+	public void testMethod1() {
+		System.out.println("prinmt test1");
+	}
+
+	public void testMethod2() {
+		System.out.println("prinmt test2");
+	}
+
+	public void testMethod3() {
+		System.out.println("prinmt test2");
+	}
+
+}
---
+++ programming/java/api/set/sortedset/ktpark/BasicInterface.java
@@ -0,0 +1,11 @@
+package java.api.set.sortedset.ktpark;
+
+public interface BasicInterface {
+
+	void testMethod1();
+
+	void testMethod2();
+
+	void testMethod3();
+
+}
