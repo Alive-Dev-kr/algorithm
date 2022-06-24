cad36854ea94e7521960e9387bc516a2c8a2d776
bhr0554 <bhr5004@gmail.com>
2022-06-25 01:33:22 +0900

    [생성] - 소수 찾기 (너무 싫어하는 형식이네)

---
+++ brj/level2/FindMinority.java
@@ -0,0 +1,24 @@
+package level2;
+
+//https://programmers.co.kr/learn/courses/30/lessons/42839
+public class FindMinority {
+
+    /**
+     * 접근 방법
+     * 1. numbers String을 쪼갠서 만들 수 있는 수들을 다 만들어 놓는다.
+     * 2. 만든 숫자 중 가장 작은 수부터 큰수까지 for문을 돌린다.
+     * 3. 그 중 소수만 골라낸다.
+     * 3-1. 2, 3, 5, 7 나누어지는 것 배제
+     *
+     */
+    private static int solution(String numbers) {
+        int answer = 0;
+
+        return answer;
+    }
+
+    public static void main(String args[]) {
+        String numbers = "17";
+        solution(numbers);
+    }
+}
